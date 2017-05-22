ed6783a76a0f739168c9d1cb65631d978556d35b
KevinLiu <k242424123@gmail.com>
2017-05-22 17:53:42 +0800

    update for going to rewrite the part of master controller add J_m and force output func

--- HumanSwarmSystem_Gazebo/HumanSwarmSystem_Gazebo.cpp
+++ HumanSwarmSystem_Gazebo/HumanSwarmSystem_Gazebo.cpp
@@ -2,15 +2,16 @@
 //
 
 #include "stdafx.h"
-//----------socket----------
-#include <WinSock2.h>	//do not put behind <Windows.h>
-#pragma comment(lib,"ws2_32.lib") //Winsock Library
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
 #define SERVER "192.168.19.73"
 #define BUFLEN 1024  //Max length of buffer
 #define PORT 1234   //The port on which to listen for incoming data
 #define SAMPLING_TIME 0.01	//unit:second
 #define DEVICE_NAME_1 "phantom1"
 #define DEVICE_NAME_2 "phantom2"
+
+//----------socket----------
+#include <WinSock2.h>	//do not put behind <Windows.h>
 
 //----------omni----------
 #include <HD\hd.h>
@@ -28,20 +29,22 @@
 # include "conio.h"
 # include <windows.h>
 #endif
-
 #include "../TimeDelayBuffer/TimeDelayBuffer.h"
 
 using namespace std;
 using namespace arma;
+#pragma comment(lib,"ws2_32.lib") //Winsock Library
 
 //--------global parameter--------
 bool DELAY_SWITCH = true;
+const int MasterRobotNum = 2;
+const int MasterRobotDOF = 3;
 const int SlaveRobotNum = 4;		//n
 const int SlaveRobotDOF = 3;		//phi
-const int TaskSpaceDimension = 6;	//m
-const int TaskSpace_variance = TaskSpaceDimension / 2;
+const int TaskSpaceDimension = MasterRobotNum * MasterRobotDOF;	//m
+const int TaskSpace_variance = TaskSpaceDimension / MasterRobotNum;
 const double u_s_saturation = 7.0;
-//const double alpha = 0.2;			//work envolope scaling factor
+const double l_1_Omni = 133.0, l_2_Omni = 133.0;
 const double MatElementTolerence = 0.00001;	//when using inv,pinv, the consequence less than this will equal to zero
 mat X_m( TaskSpaceDimension, 1, fill::zeros );
 mat X_m_last(TaskSpaceDimension, 1, fill::zeros);
@@ -66,24 +69,26 @@ mat q_s_last(SlaveRobotNum * SlaveRobotDOF, 1, fill::zeros);
 mat qDot_s( SlaveRobotNum * SlaveRobotDOF, 1, fill::zeros );
 mat qDot_s_last(SlaveRobotNum * SlaveRobotDOF, 1, fill::zeros);
 mat qDoubleDot_s(SlaveRobotNum * SlaveRobotDOF, 1, fill::zeros);
-mat J_m( TaskSpaceDimension, TaskSpaceDimension, fill::eye );
+mat J_m(TaskSpaceDimension, TaskSpaceDimension, fill::zeros);
 mat J_s( TaskSpaceDimension, SlaveRobotNum * SlaveRobotDOF, fill::zeros );
 mat J_s_last(TaskSpaceDimension, SlaveRobotNum * SlaveRobotDOF, fill::zeros);
 mat JDot_s(TaskSpaceDimension, SlaveRobotNum * SlaveRobotDOF, fill::zeros);
 mat invJ_m, pinvJ_s;
-mat Y_s;
+//mat Y_m(TaskSpaceDimension, , fill::zeros);
 mat thetaHat_s( SlaveRobotNum * SlaveRobotDOF, 1 );
 mat thetaHatDot_s(SlaveRobotNum * SlaveRobotDOF, 1, fill::zeros);
 mat f_a( SlaveRobotNum * SlaveRobotDOF, 1, fill::zeros );
 mat u_m;
 mat u_s( SlaveRobotNum * SlaveRobotDOF, 1, fill::zeros );
 mat VelocityCommand_s(SlaveRobotNum * SlaveRobotDOF, 1, fill::zeros);
+mat theta_m(MasterRobotDOF * MasterRobotNum, 1, fill::zeros);
 
 vec vk_p, vk_d, vk_t, valpha;
 mat k_p, k_d, k_t, alpha;
 double k_ss = 7.5 * 0.05;
 
 HHD DeviceID_1, DeviceID_2;
+HDdouble JointAngle_m1[MasterRobotDOF], JointAngle_m2[MasterRobotDOF];
 /*
 mat OmniBuf1(TaskSpace_variance, 3, fill::zeros);
 mat OmniBuf2(TaskSpace_variance, 3, fill::zeros);
@@ -96,27 +101,27 @@ mat X_sFilterBuf(TaskSpaceDimension, 3, fill::zeros);
 int X_sFilterBufFlag = 0;
 */
 
-double l_1_Omni = 133.0, l_2_Omni = 133.0;
 //--------function--------
 mat timeDerivative(mat last, mat now);
 vec medianFilter(mat* dataBuf, mat* newData, int* BufFlag);
 HDCallbackCode HDCALLBACK GetCmd1Callback(void *data);
 HDCallbackCode HDCALLBACK GetCmd2Callback(void *data);
+HDCallbackCode HDCALLBACK ForceOutput1Callback(void *data);
+HDCallbackCode HDCALLBACK ForceOutput2Callback(void *data);
 void printMatToTxt(FILE *dst, mat *src);
-char* toSendBuf(string src);
+char* stringToChar_heap(string src);
 //-----------------------------------------
 int _tmain(int argc, _TCHAR* argv[])
 {
+	Sleep(500);	
+	//--------initialize--------
 	char* sendtoBuf = NULL;
 	stringstream ss;
 	string string_converted;
 
-	Sleep(500);	
-	//--------initialize--------
 	thetaHat_s.fill( 0.15 );
 	valpha << 0.2 << 0.2 << 0.2 << 0.2 << 0.2 << 0.2;
 	alpha = diagmat(valpha);
-
 	/*
 	vk_p << 0.0000125 << 0.0000125 << 0.0000125 << 0.0000075 << 0.0000075 << 0.0000075;
 	k_p = diagmat(vk_p);
@@ -124,8 +129,6 @@ int _tmain(int argc, _TCHAR* argv[])
 	k_d = diagmat(vk_d);
 	vk_t << 0.02 << 0.02 << 0.02 << 0.2 << 0.2 << 0.2;
 	k_t = diagmat(vk_t);*/
-	
-	
 	vk_p << 0.000125 << 0.000125 << 0.000125 << 0.000075 << 0.000075 << 0.000075;
 	k_p = diagmat(vk_p);
 	//vk_d << 0.00125 << 0.00125 << 0.00125 << 0.0001 << 0.0001 << 0.0001;
@@ -207,16 +210,20 @@ int _tmain(int argc, _TCHAR* argv[])
 
 	//	set omni & scheduler
 	DeviceID_1 = hdInitDevice(DEVICE_NAME_1);
+	hdEnable(HD_FORCE_OUTPUT);
 	DeviceID_2 = hdInitDevice(DEVICE_NAME_2);
+	hdEnable(HD_FORCE_OUTPUT);
 	hdStartScheduler();
 	HDSchedulerHandle Scheduler_GetCmd1 = hdScheduleAsynchronous(GetCmd1Callback, (void*) 0, HD_MAX_SCHEDULER_PRIORITY);
 	HDSchedulerHandle Scheduler_GetCmd2 = hdScheduleAsynchronous(GetCmd2Callback, (void*) 0, HD_MAX_SCHEDULER_PRIORITY);
+	HDSchedulerHandle Scheduler_ForceOutput1 = hdScheduleAsynchronous(ForceOutput1Callback, (void*)0, HD_MAX_SCHEDULER_PRIORITY);
+	HDSchedulerHandle Scheduler_ForceOutput2 = hdScheduleAsynchronous(ForceOutput2Callback, (void*)0, HD_MAX_SCHEDULER_PRIORITY);
 	
 	//	use sendto() before using recvfrom() to implicitly bind
 	for (int i = 0; i < SlaveRobotDOF * SlaveRobotNum; i++)
 		ss << "0 ";
 	//string_converted = ss.str();
-	sendtoBuf = toSendBuf(ss.str());
+	sendtoBuf = stringToChar_heap(ss.str());
 	ss.str("");
 	ss.clear();
 	/*
@@ -244,7 +251,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	FILE* delay_s_file = fopen("..\\record\\delay_s.txt", "w");
 	
 	//time delay buf
-	DelayedBuffer TimeDelayBuffer_m_to_s = DelayedBuffer(TaskSpaceDimension, 1);
+	DelayedBuffer *TimeDelayBuffer_m_to_s = NULL;
 	DelayedData data_m_to_s;
 	int delay_m_to_s;	//unit:ms
 
@@ -256,20 +263,16 @@ int _tmain(int argc, _TCHAR* argv[])
 		 * part1:get master and slave robots information to update q,qDot,X...
 		 */
 
-		//SYSTEMTIME LoopStart;
-		//GetLocalTime(&LoopStart);
 		char buf_from_slave[BUFLEN];
 		int timer_loop_start, timer_loop_end;
 
 		timer_loop_start = clock();
-		//delay_m_to_s = 1000 + 1000 * sin(0.0001*timer_loop_start);
-		delay_m_to_s = 1000;
+		delay_m_to_s = 1000 + 1000 * sin(0.0001*timer_loop_start);
 		ss << delay_m_to_s << endl;
 		string_converted = ss.str();
 		fprintf(delay_s_file, string_converted.c_str());
 		ss.str("");
-		ss.clear();
-		
+		ss.clear();		
 
 		//	store value for calculating time derivative
 		J_s_last = J_s;
@@ -332,21 +335,32 @@ int _tmain(int argc, _TCHAR* argv[])
 			X_s(axis + TaskSpace_variance, 0) = VarSum / (double)SlaveRobotNum;
 		}
 
-		//	update X_m
+		//	update X_m ,theta_m ,q_m
 		X_m = X_m_temp;	//don't put this just after hdWaitForCompletion,
 						//or the callback could not be completed yet
 						//and the value might be changed unexpected
-		/*
+		theta_m(0, 0) = JointAngle_m1[0];	//master1
+		theta_m(1, 0) = JointAngle_m1[1];
+		theta_m(2, 0) = JointAngle_m1[2] - JointAngle_m1[1];
+		theta_m(3, 0) = JointAngle_m2[0];	//master2
+		theta_m(4, 0) = JointAngle_m2[1];
+		theta_m(5, 0) = JointAngle_m2[2] - JointAngle_m2[1];
+		for (int i = 0; i < q_m.rows; i++)
+			q_m(i, 0) = theta_m(i, 0);
+
+/*
 		//filter q_s
 		if (abs(X_s - X_s_last).max() < 0.01)
-			X_s = X_s_last;*/
-
+			X_s = X_s_last;
+*/
 		if (loopCount < 2)
 		{
 			X_m_initial = X_m;
 			X_s_initial = X_s;
+			//q_m_initail = q_m;
+/*
 			//median filter
-			/*X_mFilterBuf.col(X_mFilterBufFlag) = X_m;
+			X_mFilterBuf.col(X_mFilterBufFlag) = X_m;
 			X_mFilterBufFlag++;
 			X_sFilterBuf.col(X_sFilterBufFlag) = X_s;
 			X_sFilterBufFlag++;
@@ -354,7 +368,8 @@ int _tmain(int argc, _TCHAR* argv[])
 		else
 		{
 			X_m = medianFilter(&X_mFilterBuf, &X_m, &X_mFilterBufFlag);
-			X_s = medianFilter(&X_sFilterBuf, &X_s, &X_sFilterBufFlag);*/
+			X_s = medianFilter(&X_sFilterBuf, &X_s, &X_sFilterBufFlag);
+*/
 		}
 
 		//	shift master coordinate to match slave initial condition
@@ -370,14 +385,14 @@ int _tmain(int argc, _TCHAR* argv[])
 		}
 		printMatToTxt(X_m_no_delay_file, &X_m);
 		if (loopCount == 0)
-			TimeDelayBuffer_m_to_s = DelayedBuffer(DelayedData(X_m, 0, 0));
+			TimeDelayBuffer_m_to_s = new DelayedBuffer(DelayedData(X_m, 0, 0));
 		
 		//time delay
 		if (DELAY_SWITCH)
 		{
 			data_m_to_s = DelayedData(X_m, loopCount, timer_loop_start + delay_m_to_s);
-			TimeDelayBuffer_m_to_s.addData(data_m_to_s);	//put current data into buffer
-			X_m = TimeDelayBuffer_m_to_s.getData(timer_loop_start);	//get arrived data from buffer
+			TimeDelayBuffer_m_to_s->addData(data_m_to_s);	//put current data into buffer
+			X_m = TimeDelayBuffer_m_to_s->getData(timer_loop_start);	//get arrived data from buffer
 			//cout << "size of buf: " << TimeDelayBuffer_m_to_s.buf.capacity() << endl;
 		}
 		
@@ -403,7 +418,53 @@ int _tmain(int argc, _TCHAR* argv[])
 
 		eDot_s = timeDerivative(e_s_last, e_s);
 		//e_s.t().print("e_s");
-		//J_m = ;
+		//	J_m
+		J_m(0, 0) = -l_1_Omni*cos(theta_m(0, 0)) * cos(theta_m(1, 0))	//master1
+			- l_2_Omni * cos(theta_m(0, 0)) * cos(theta_m(1, 0)) * sin(theta_m(2, 0))
+			- l_2_Omni * cos(theta_m(0, 0)) * sin(theta_m(1, 0)) * cos(theta_m(2, 0));
+		J_m(0, 1) = l_2_Omni * sin(theta_m(0, 0)) * sin(theta_m(1, 0)) * sin(theta_m(2, 0))
+			+ l_1_Omni *sin(theta_m(0, 0)) * sin(theta_m(1, 0))
+			- l_2_Omni * sin(theta_m(0, 0)) *cos(theta_m(1, 0)) * cos(theta_m(2, 0));
+		J_m(0,2) = -l_2_Omni * sin(theta_m(0, 0)) * cos(theta_m(1, 0)) * cos(theta_m(2, 0)) 
+			+ l_2_Omni * sin(theta_m(0, 0)) * sin(theta_m(1, 0)) * sin(theta_m(2, 0));
+		J_m(1,0) = 0;
+		J_m(1, 1) = l_2_Omni * sin(theta_m(1, 0)) * cos(theta_m(2, 0)) 
+			+ l_2_Omni * cos(theta_m(1, 0)) * sin(theta_m(2, 0)) 
+			+ l_1_Omni  *  cos(theta_m(1, 0));
+		J_m(1,2) = l_2_Omni * cos(theta_m(1, 0)) * sin(theta_m(2, 0)) 
+			+ l_2_Omni * sin(theta_m(1, 0)) * cos(theta_m(2, 0));
+		J_m(2,0) = -l_2_Omni * sin(theta_m(0, 0)) * sin(theta_m(1, 0)) * cos(theta_m(2, 0)) 
+			- l_1_Omni * sin(theta_m(0, 0)) * cos(theta_m(1, 0)) 
+			- l_2_Omni * sin(theta_m(0, 0)) * cos(theta_m(1, 0)) * sin(theta_m(2, 0));
+		J_m(2,1) = l_2_Omni * cos(theta_m(0, 0)) * cos(theta_m(1, 0)) * cos(theta_m(2, 0)) 
+			- l_2_Omni * cos(theta_m(0, 0)) * sin(theta_m(1, 0)) * sin(theta_m(2, 0)) 
+			- l_1_Omni * cos(theta_m(0, 0)) * sin(theta_m(1, 0));
+		J_m(2,2) = l_2_Omni * cos(theta_m(0, 0)) * cos(theta_m(1, 0)) * cos(theta_m(2, 0)) 
+			- l_2_Omni * cos(theta_m(0, 0)) * sin(theta_m(1, 0)) * sin(theta_m(2, 0));
+
+		J_m(3, 3) = -l_1_Omni*cos(theta_m(3, 0)) * cos(theta_m(4, 0))	//master2
+			- l_2_Omni * cos(theta_m(3, 0)) * cos(theta_m(4, 0)) * sin(theta_m(5, 0))
+			- l_2_Omni * cos(theta_m(3, 0)) * sin(theta_m(4, 0)) * cos(theta_m(5, 0));
+		J_m(3, 4) = l_2_Omni * sin(theta_m(3, 0)) * sin(theta_m(4, 0)) * sin(theta_m(5, 0))
+			+ l_1_Omni *sin(theta_m(3, 0)) * sin(theta_m(4, 0))
+			- l_2_Omni * sin(theta_m(3, 0)) *cos(theta_m(4, 0)) * cos(theta_m(5, 0));
+		J_m(3, 5) = -l_2_Omni * sin(theta_m(3, 0)) * cos(theta_m(4, 0)) * cos(theta_m(5, 0))
+			+ l_2_Omni * sin(theta_m(3, 0)) * sin(theta_m(4, 0)) * sin(theta_m(5, 0));
+		J_m(4, 3) = 0;
+		J_m(4, 4) = l_2_Omni * sin(theta_m(4, 0)) * cos(theta_m(5, 0))
+			+ l_2_Omni * cos(theta_m(4, 0)) * sin(theta_m(5, 0))
+			+ l_1_Omni  *  cos(theta_m(4, 0));
+		J_m(4, 5) = l_2_Omni * cos(theta_m(4, 0)) * sin(theta_m(5, 0))
+			+ l_2_Omni * sin(theta_m(4, 0)) * cos(theta_m(5, 0));
+		J_m(5, 3) = -l_2_Omni * sin(theta_m(3, 0)) * sin(theta_m(4, 0)) * cos(theta_m(5, 0))
+			- l_1_Omni * sin(theta_m(3, 0)) * cos(theta_m(4, 0))
+			- l_2_Omni * sin(theta_m(3, 0)) * cos(theta_m(4, 0)) * sin(theta_m(5, 0));
+		J_m(5, 4) = l_2_Omni * cos(theta_m(3, 0)) * cos(theta_m(4, 0)) * cos(theta_m(5, 0))
+			- l_2_Omni * cos(theta_m(3, 0)) * sin(theta_m(4, 0)) * sin(theta_m(5, 0))
+			- l_1_Omni * cos(theta_m(3, 0)) * sin(theta_m(4, 0));
+		J_m(5, 5) = l_2_Omni * cos(theta_m(3, 0)) * cos(theta_m(4, 0)) * cos(theta_m(5, 0))
+			- l_2_Omni * cos(theta_m(3, 0)) * sin(theta_m(4, 0)) * sin(theta_m(5, 0));
+
 		printMatToTxt(e_s_file, &e_s);
 
 		mat SlaveAveragePosition(SlaveRobotDOF, 1, fill::zeros);
@@ -445,7 +506,7 @@ int _tmain(int argc, _TCHAR* argv[])
 			}
 		}
 
-		//invJ_m = inv( J_m, MatElementTolerence );
+		invJ_m = inv( J_m, MatElementTolerence );
 		pinvJ_s = pinv( J_s, MatElementTolerence );
 		//	secondary task
 		//	maximize inner distance
@@ -491,7 +552,7 @@ int _tmain(int argc, _TCHAR* argv[])
 		//thetaHatDot_s = -1 * diagmat(Y_s).t() * s_s;
 		if (loopCount >= 5)
 		{
-			//thetaHat_s += thetaHatDot_s * SAMPLING_TIME;
+			//thetaHat_m += thetaHatDot_m * SAMPLING_TIME;
 			//u_m = ;
 			//u_s = diagmat(Y_s) * thetaHat_s - k_ss * s_s - trans(J_s) * k_p * (-k_t * e_s + XDot_s);
 			u_s = (qDoubleDot_s - timeDerivative(s_s_last, s_s)) - k_ss * s_s - trans(J_s) * k_p * J_s * s_s;
@@ -524,6 +585,13 @@ int _tmain(int argc, _TCHAR* argv[])
 		 * part3:send command to robots
 		 */
 		loopCount++;
+		// to master
+		if (!hdWaitForCompletion(Scheduler_ForceOutput1, HD_WAIT_CHECK_STATUS) || !hdWaitForCompletion(Scheduler_ForceOutput2, HD_WAIT_CHECK_STATUS))
+		{
+			fprintf(stderr, "Press any key to quit.\n");
+			_getch();
+			break;
+		}
 		//	get enough data for median filter in the beginning and send command to keep remote UDP working(for blocking)
 		
 		/*
@@ -546,8 +614,7 @@ int _tmain(int argc, _TCHAR* argv[])
 				ss << VelocityCommand_s(num * SlaveRobotDOF + axis, 0) << " ";
 			}
 		}
-		sendtoBuf = toSendBuf(ss.str());
-		//string_converted = ss.str();
+		sendtoBuf = stringToChar_heap(ss.str());
 		//	clear stringstream
 		ss.str("");
 		ss.clear();
@@ -567,10 +634,14 @@ int _tmain(int argc, _TCHAR* argv[])
 			Sleep(until_sampling);
 	}
 
-	//--------end--------	
+	//--------end--------
+	delete TimeDelayBuffer_m_to_s;
+	TimeDelayBuffer_m_to_s = NULL;
 	hdStopScheduler();
 	hdUnschedule(Scheduler_GetCmd1);
 	hdUnschedule(Scheduler_GetCmd2);
+	hdUnschedule(Scheduler_ForceOutput1);
+	hdUnschedule(Scheduler_ForceOutput2);
 	hdDisableDevice(DeviceID_1);
 	hdDisableDevice(DeviceID_2);
 	closesocket(s);
@@ -594,6 +665,7 @@ HDCallbackCode HDCALLBACK GetCmd1Callback(void *data)
 	//	read command from omni
 	hdBeginFrame(DeviceID_1);
 	hdGetDoublev(HD_CURRENT_POSITION, command);
+	hdGetDoublev(HD_CURRENT_JOINT_ANGLES, JointAngle_m1);
 	for (int axis = 0; axis < TaskSpace_variance; axis++)
 	{
 		X_m_temp(axis, 0) = command[axis];
@@ -610,6 +682,7 @@ HDCallbackCode HDCALLBACK GetCmd2Callback(void *data)
 	//	read command from omni
 	hdBeginFrame(DeviceID_2);
 	hdGetDoublev(HD_CURRENT_POSITION, command);
+	hdGetDoublev(HD_CURRENT_JOINT_ANGLES, JointAngle_m2);
 	for (int axis = 0; axis < TaskSpace_variance; axis++)
 	{
 		X_m_temp(3 + axis, 0) = command[axis];
@@ -648,9 +721,39 @@ void printMatToTxt(FILE *dst, mat *src)
 	fprintf(dst, "\n");
 }
 
-char* toSendBuf(string src)
+char* stringToChar_heap(string src)
 {
+	//	use heap memory for sendto() buf
+	//	remember to delete
 	char* buf = new char[src.size()+1];
 	strcpy(buf, src.c_str());
 	return buf;
+}
+
+HDCallbackCode HDCALLBACK ForceOutput1Callback(void *data)
+{
+	HDdouble force[MasterRobotDOF];
+	
+	hdBeginFrame(DeviceID_1);
+	for (int axis = 0; axis < MasterRobotDOF; axis++)
+		force[axis] = u_m(axis, 0);
+
+	hdSetDoublev(HD_FORCE_OUTPUT, force);
+	hdEndFrame(DeviceID_1);
+
+	return HD_CALLBACK_CONTINUE;
+}
+
+HDCallbackCode HDCALLBACK ForceOutput2Callback(void *data)
+{
+	HDdouble force[MasterRobotDOF];
+
+	hdBeginFrame(DeviceID_2);
+	for (int axis = 0; axis < MasterRobotDOF; axis++)
+		force[axis] = u_m(MasterRobotDOF + axis, 0);
+
+	hdSetDoublev(HD_FORCE_OUTPUT, force);
+	hdEndFrame(DeviceID_2);
+
+	return HD_CALLBACK_CONTINUE;
 }
